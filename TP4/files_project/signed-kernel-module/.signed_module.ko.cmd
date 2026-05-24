dfc6c3c85af62899d2be18e67183e5611aae1e76
TomasBrigido <tomas.brigido@mi.unc.edu.ar>
2026-05-24 00:43:32 -0300

    Signed module

---
+++ TP4/files_project/signed-kernel-module/.signed_module.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_signed_module.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.17.0-29-generic/scripts/module.lds -o signed_module.ko signed_module.o signed_module.mod.o .module-common.o
---
+++ TP4/files_project/signed-kernel-module/signed_module.mod
@@ -0,0 +1,1 @@
+./signed_module.o
